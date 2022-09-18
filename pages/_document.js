43c4a9b496b058d24dcbf590206221447ab9500a
saki <16546851+sakiko-2@users.noreply.github.com>
2022-09-19 11:48:28 +1200

    Change font-display to swap

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default function Document() {
         <meta name="description" content="Web site to search a country" />
         <link rel="icon" href="/favicon.ico" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&display=optional"
+          href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&display=swap"
           rel="stylesheet"
         />
       </Head>
